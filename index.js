f0224faace2d0ee7fc9f633dc0149893d82e93e8
iamharie <haranhari350@gmail.com>
2024-04-04 00:08:04 -0500

    Pushing my portfolio to GitHub

---
+++ index.js
@@ -0,0 +1,8 @@
+const name = document.querySelector(".name");
+const email = document.querySelector(".email");
+const text = document.querySelector(".trxt-area");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function () {
+  alert("your response was sent successfully");
+});
